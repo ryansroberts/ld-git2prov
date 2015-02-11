63b4f771bd274a9deeda803221329c6ca3a965f6
Ryan Roberts <ryansroberts@gmail.com>
2015-02-11 16:40:31 +0000

    Pretty printing

--- src/git2prov/Git.fs
+++ src/git2prov/Git.fs
@@ -1,83 +1,73 @@
 module Git
+
 open LibGit2Sharp
 open System.IO
 
-type Commit =
-  | Commit of LibGit2Sharp.Commit
+type Commit = 
+    | Commit of LibGit2Sharp.Commit
 
-type Repository =
-  | Repository of LibGit2Sharp.Repository
+type Repository = 
+    | Repository of LibGit2Sharp.Repository
 
-type WorkingArea =
-  | WorkingArea of StatusEntry list * Commit
+type WorkingArea = 
+    | WorkingArea of StatusEntry list * Commit
 
-type Content =
-  | Text of string
-  | NoContent
+type Content = 
+    | Text of string
+    | NoContent
 
-type Path =
-  | Path of string
+type Path = 
+    | Path of string
 
-let repo p = Repository  ( new LibGit2Sharp.Repository (p) )
+let repo p = Repository(new LibGit2Sharp.Repository(p))
 
-let commits t (r:Repository) =
-  match r with
-  | Repository r -> seq {
-      for c in r.Commits.QueryBy (CommitFilter (SortBy=CommitSortStrategies.Topological,Since=t)) do
-      yield Commit c
-     }
+let commits t (r : Repository) = 
+    match r with
+    | Repository r -> 
+        seq { 
+            for c in r.Commits.QueryBy
+                         (CommitFilter
+                              (SortBy = CommitSortStrategies.Topological, 
+                               Since = t)) do
+                yield Commit c
+        }
 
-let tree = function
-  | Commit c -> c.Tree
-  
-let workingArea = function
-  | Repository r ->
-    let s = r.RetrieveStatus ()
-    WorkingArea (Seq.concat
-     [
-      s.Added
-      s.Modified
-      s.Untracked
-      s.Staged
-      s.RenamedInIndex
-      s.RenamedInWorkDir
-     ] |> Seq.toList,Commit r.Head.Tip)
-
-let diff (c,c') = function
-  | Repository r ->
-    r.Diff.Compare<TreeChanges>(oldTree=c,newTree=c')
-
-let diffs cx = function
-  | Repository r ->
-    cx
-    |> Seq.map tree
-    |> Seq.pairwise
-    |> Seq.map diff
-
-let branchName = function
-  | Repository r ->
-    match r.Head.Name with
-      | null -> r.Head.Tip.Sha
-      | name -> name
-
-let workingDirectory = function
-  | Repository r ->
-    Path r.Info.WorkingDirectory
-
-let directoryName = function
-  | Path p ->
-    p.Split (Path.DirectorySeparatorChar)
-    |> Seq.last
-
-let content (h:string) p = function
-  | Repository r -> 
-    let c = r.Lookup<LibGit2Sharp.Commit> h
-    match c.[p] with
-      | null -> NoContent
-      | tr ->
-        match tr.TargetType with
-          | LibGit2Sharp.TreeEntryTargetType.Blob ->
-            let b = tr.Target :?> LibGit2Sharp.Blob
-            use r = new StreamReader(b.GetContentStream())
-            Text (r.ReadToEnd ())
-          | _ -> NoContent
+let tree = function 
+    | Commit c -> c.Tree
+let workingArea = 
+    function 
+    | Repository r -> 
+        let s = r.RetrieveStatus()
+        WorkingArea
+            (Seq.concat 
+                 [ s.Added; s.Modified; s.Untracked; s.Staged; s.RenamedInIndex; 
+                   s.RenamedInWorkDir ] |> Seq.toList, Commit r.Head.Tip)
+let diff (c, c') = function 
+    | Repository r -> r.Diff.Compare<TreeChanges>(oldTree = c, newTree = c')
+let diffs cx = function 
+    | Repository r -> 
+        cx
+        |> Seq.map tree
+        |> Seq.pairwise
+        |> Seq.map diff
+let branchName = function 
+    | Repository r -> 
+        match r.Head.Name with
+        | null -> r.Head.Tip.Sha
+        | name -> name
+let workingDirectory = function 
+    | Repository r -> Path r.Info.WorkingDirectory
+let directoryName = function 
+    | Path p -> p.Split(Path.DirectorySeparatorChar) |> Seq.last
+let content (h : string) p = function 
+    | Repository r -> 
+        let c = r.Lookup<LibGit2Sharp.Commit> h
+        match c.[p] with
+        | null -> NoContent
+        | tr -> 
+            match tr.TargetType with
+            | LibGit2Sharp.TreeEntryTargetType.Blob -> 
+                let b = tr.Target :?> LibGit2Sharp.Blob
+                use r = new StreamReader(b.GetContentStream())
+                Text(r.ReadToEnd())
+            | _ -> NoContent
--- src/git2prov/Prov.fs
+++ src/git2prov/Prov.fs
@@ -1,90 +1,97 @@
-module Prov 
-  open Git
-  open System
+module Prov
 
-  let short sha = function
-    | Repository r ->
-      r.ObjectDatabase.ShortenObjectId sha
+open Git
+open System
 
-  let iso (date:DateTimeOffset) =
-        date.ToString("o")
- 
-  type Uri =
-    | Uri of string 
-    with static member commit r c = Uri (sprintf "git2prov:commit/%s" (short c r))
-         static member compilation = Uri (sprintf "compilation:compilation/%s" (iso System.DateTimeOffset.Now))
-         static member workingarea = Uri "git2prov:workingarea" 
-         static member identity (c:LibGit2Sharp.Commit) = Uri (sprintf "git2prov:user/%s" c.Author.Email)
-         static member identity () = Uri (sprintf "git2prov:user/%s" System.Environment.UserName)
-         static member versionedcontent (c,p) = Uri (sprintf "git2prov:commit/%s/%s" c p)
-         static member specialisationOf (r,p) = Uri (sprintf "base:%s" p)
-         override x.ToString () = match x with | Uri s -> s
-         
-  type FileVersion = {
-    Id : Uri
-    Content: Git.Content
-    PreviousVersion : string option
-    SpecialisationOf : Uri
-    Commit : Uri
-    AttributedTo : Uri
-    }
-  with static member from (c:LibGit2Sharp.Commit,c':LibGit2Sharp.Commit,r) = seq {
-        let d = diff (c.Tree,c'.Tree) r
-        for f in d.Modified do yield {
-          Id = Uri.versionedcontent (f.Oid.Sha,f.Path)
-          Content = Git.content c.Id.Sha f.Path r
-          PreviousVersion = Some f.OldOid.Sha 
-          SpecialisationOf = Uri.specialisationOf (r,f.Path) 
-          Commit = Uri.commit r c
-          AttributedTo = Uri.identity c 
-          }
+let short sha = function 
+    | Repository r -> r.ObjectDatabase.ShortenObjectId sha
+let iso (date : DateTimeOffset) = date.ToString("o")
+
+type Uri = 
+    | Uri of string
+    static member commit r c = Uri(sprintf "git2prov:commit/%s" (short c r))
+    static member compilation = 
+        Uri
+            (sprintf "compilation:compilation/%s" 
+                 (iso System.DateTimeOffset.Now))
+    static member workingarea = Uri "git2prov:workingarea"
+    static member identity (c : LibGit2Sharp.Commit) = 
+        Uri(sprintf "git2prov:user/%s" c.Author.Email)
+    static member identity() = 
+        Uri(sprintf "git2prov:user/%s" System.Environment.UserName)
+    static member versionedcontent (c, p) = 
+        Uri(sprintf "git2prov:commit/%s/%s" c p)
+    static member specialisationOf (r, p) = Uri(sprintf "base:%s" p)
+    override x.ToString() = 
+        match x with
+        | Uri s -> s
+
+type FileVersion = 
+    { Id : Uri
+      Content : Git.Content
+      PreviousVersion : string option
+      SpecialisationOf : Uri
+      Commit : Uri
+      AttributedTo : Uri }
+    
+    static member from (c : LibGit2Sharp.Commit, c' : LibGit2Sharp.Commit, r) = 
+        seq { 
+            let d = diff (c.Tree, c'.Tree) r
+            for f in d.Modified do
+                yield { Id = Uri.versionedcontent (f.Oid.Sha, f.Path)
+                        Content = Git.content c.Id.Sha f.Path r
+                        PreviousVersion = Some f.OldOid.Sha
+                        SpecialisationOf = Uri.specialisationOf (r, f.Path)
+                        Commit = Uri.commit r c
+                        AttributedTo = Uri.identity c }
         }
-       static member from (wx:LibGit2Sharp.StatusEntry list,r) = seq {
-         for f in wx do yield {
-           Id = Uri f.FilePath
-           Content = Git.Content.Text (System.IO.File.ReadAllText (f.FilePath))
-           PreviousVersion = None
-           SpecialisationOf = Uri.specialisationOf (r,f.FilePath)
-           Commit = Uri.workingarea
-           AttributedTo = Uri System.Environment.UserName
-         }
-       } 
-  
-  type Activity = {
-    Id : Uri
-    Time : DateTimeOffset
-    Label : string
-    User : Uri
-    Used : FileVersion seq
-    InformedBy : Uri list 
-    }
-  with static member fromCommit r = function
-        | Commit c,Commit c' -> {
-            Id = Uri.commit r c
-            Time = c.Author.When
-            Label = c.Message
-            User = Uri.identity c
-            Used = FileVersion.from (c,c',r)
-            InformedBy = [for p in c.Parents -> Uri.commit r p]
-          }
-       static member fromWorkingArea r = function
-        | WorkingArea (wx,Commit c) -> {
-            Id = Uri.workingarea
-            Time = DateTimeOffset.Now
-            Label = "Uncommitted changes from working area"
-            User = Uri.identity () 
-            Used = FileVersion.from (wx,r)
-            InformedBy = [Uri.commit r c]
-          } 
-       static member concat ax = {
-         Id = Uri.compilation
-         Time = DateTimeOffset.Now
-         Label = "Compilation message that is actualy useful"
-         User = Uri.identity ()
-         Used = ax
-            |> Seq.map (fun a -> a.Used)
-            |> Seq.concat
-            |> Seq.groupBy (fun a -> a.SpecialisationOf)
-            |> Seq.map (fun (_,dx) -> dx |> Seq.last)
-         InformedBy = []   
-         }
+    
+    static member from (wx : LibGit2Sharp.StatusEntry list, r) = 
+        seq { 
+            for f in wx do
+                yield { Id = Uri f.FilePath
+                        Content = 
+                            Git.Content.Text
+                                (System.IO.File.ReadAllText(f.FilePath))
+                        PreviousVersion = None
+                        SpecialisationOf = Uri.specialisationOf (r, f.FilePath)
+                        Commit = Uri.workingarea
+                        AttributedTo = Uri System.Environment.UserName }
+        }
+
+type Activity = 
+    { Id : Uri
+      Time : DateTimeOffset
+      Label : string
+      User : Uri
+      Used : FileVersion seq
+      InformedBy : Uri list }
+    static member fromCommit r = function 
+        | Commit c, Commit c' -> 
+            { Id = Uri.commit r c
+              Time = c.Author.When
+              Label = c.Message
+              User = Uri.identity c
+              Used = FileVersion.from (c, c', r)
+              InformedBy = 
+                  [ for p in c.Parents -> Uri.commit r p ] }
+    static member fromWorkingArea r = function 
+        | WorkingArea(wx, Commit c) -> 
+            { Id = Uri.workingarea
+              Time = DateTimeOffset.Now
+              Label = "Uncommitted changes from working area"
+              User = Uri.identity()
+              Used = FileVersion.from (wx, r)
+              InformedBy = [ Uri.commit r c ] }
+    static member concat ax = 
+        { Id = Uri.compilation
+          Time = DateTimeOffset.Now
+          Label = "Compilation message that is actualy useful"
+          User = Uri.identity()
+          Used = 
+              ax
+              |> Seq.map (fun a -> a.Used)
+              |> Seq.concat
+              |> Seq.groupBy (fun a -> a.SpecialisationOf)
+              |> Seq.map (fun (_, dx) -> dx |> Seq.last)
+          InformedBy = [] }

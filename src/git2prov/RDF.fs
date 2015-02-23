5c7e44a43390435d41e4c6339862ff752a499e17
Ryan Roberts <ryansroberts@gmail.com>
2015-02-23 16:00:26 +0000

    And don't forget the trailing slash :|

--- src/git2prov/RDF.fs
+++ src/git2prov/RDF.fs
@@ -20,7 +20,7 @@ module RDF =
         let owl = "http://www.w3.org/2002/07/owl#"
         let cnt = "http://www.w3.org/2011/content#"
         let compilation = "http://nice.org.uk/ns/compilation#"
-        let git2prov = "http://nice.org.uk/ns/prov"
+        let git2prov = "http://nice.org.uk/ns/prov/"
         
         let add (g : IGraph, baseUri) = 
             g.BaseUri <- UriFactory.Create baseUri

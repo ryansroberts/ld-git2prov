3f4247bb6728dc6cc017045f82c2baca84189403
Ryan Roberts <ryansroberts@gmail.com>
2015-03-26 14:27:08 +0000

    Nuget from drone

--- build.fsx
+++ build.fsx
@@ -274,7 +274,7 @@ Target "BuildPackage" (fun _ ->
   let v = sprintf "1.0.%s" n
   [
    ("mono",".paket/paket.exe pack output . version " + v)
-   ("mono",sprintf ".paket/paket.exe push url https://www.nuget.org file NICE.git2prov.%s.nupkg" v)
+   ("mono",sprintf ".paket/paket.exe push apikey $DRONE_NUGETKEY url https://www.nuget.org file NICE.git2prov.%s.nupkg" v)
   ]
   |> List.iter (fun (v,a) -> Shell.Exec (v,args=a) |> ignore)
 )

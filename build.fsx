a007bc0d9b3626ed7fd9c7bc346bb9f63b623118
Ryan Roberts <ryansroberts@gmail.com>
2015-04-28 11:37:13 +0100

    Bump version

--- build.fsx
+++ build.fsx
@@ -255,7 +255,7 @@ Target "Release" (fun _ ->
                   |> Async.RunSynchronously)
 Target "BuildPackage" (fun _ ->
   let n = Environment.GetEnvironmentVariable "DRONE_BUILD_NUMBER"
-  let v = sprintf "1.0.%s" n
+  let v = sprintf "1.2.%s" n
   [
    ("mono",".paket/paket.exe pack output . version " + v)
    ("mono",sprintf ".paket/paket.exe push apikey f3709835-b06d-444a-87dc-18786597f812 url https://www.nuget.org file NICE.git2prov.%s.nupkg" v)
